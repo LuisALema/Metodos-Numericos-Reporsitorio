5df1303a527fe2a029ad610a3c9ccace79f6f5b8
LuisALema <118556807+LuisALema@users.noreply.github.com>
2024-12-02 08:59:28 -0500

    Create Deber7.py

---
+++ DEBER_OPCIONAL7/Deber7.py
@@ -0,0 +1,14 @@
+#"python.formatting.provider": "autopep8",
+#"python.formatting.provider": "black",
+#"python.formatting.provider": "yapf",
+"python.formatting.provider": "",
+""" "editor.codeActionsOnSave": {
+    "source.organizeImports": true
+}"""
+
+import os,sys
+def   mi_funcion(x,y):return x+y
+def otraFuncion(): print( "Hola Mundo" );  return   42
+class MiClase:
+ def __init__(self,valor): self.valor=valor
+ def metodo(self,numero):return self.valor*numero   
